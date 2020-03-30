ef7ef131c8555547362118cb05853cb1f48b266c
Carlos Vargas <cornielleandres@gmail.com>
2020-03-30 19:35:44 -0400

    Get locations by radius.

--- models/locationModel.js
+++ models/locationModel.js
@@ -1,8 +1,11 @@
 const db = require("../data/dbConfig");
 
 module.exports = {
-	getLocationById,
-	create,
+  getLocationById,
+  getLocationsByRadius,
+  create,
+  update,
+  remove,
 };
 
 function getLocationById(id) {
@@ -11,10 +14,41 @@ function getLocationById(id) {
     .first();
 };
 
-function create(location) {
+function getLocationsByRadius(lat, lon, radius) {
+  // 0.08 === 5 miles
+  const extent = radius * 0.08 / 5;
+  const lowerLatLimit = lat - extent;
+  const upperLatLimit = lat + extent;
+  const lowerLonLimit = lon - extent;
+  const upperLonLimit = lon + extent;
+
+  return db("locations")
+    .where("latitude", ">", lowerLatLimit)
+    .andWhere("latitude", "<", upperLatLimit)
+    .andWhere("longitude", ">", lowerLonLimit)
+    .andWhere("longitude", "<", upperLonLimit);
+};
+
+async function create(location) {
+  const { latitude, longitude } = location;
+  const previousLocation = await db("locations").where({ latitude, longitude }).first();
+  if (previousLocation) {
+    throw new Error(`Location at latitude '${ latitude }' and longitude '${ longitude }' already exists.`);
+  }
   return db("locations")
     .insert(location, "id")
-    .then(res => {
-      return getLocationById(res[0]);
-    });
+    .then(res => getLocationById(res[0]));
+};
+
+function update(id, changes) {
+  return db("locations")
+    .where({ id })
+    .update(changes)
+    .then(res => getLocationById(id));
+};
+
+function remove(id) {
+  return db("locations")
+    .where({ id })
+    .del();
 };
--- routes/locationRoute.js
+++ routes/locationRoute.js
@@ -4,13 +4,34 @@ const { Location } = require("../models/index");
 
 const router = express.Router();
 
-router.get("/:id", async (req, res) => {
+router.get("/id/:id", async (req, res) => {
   const id = req.params.id;
 
-  const location = await Location.getLocationById(id);
+  try {
+    const location = await Location.getLocationById(id);
+    if (location) return res.status(200).json(location);
+    return res.status(404).json({ errorMessage: "No location with that ID" });
+  } catch (e) {
+    return res.status(500).json({ errorMessage: `Error getting location: ${ e }` });
+  }
+});
 
-  if (location) return res.status(200).json(location);
-  return res.status(404).json({ errorMessage: "No location with that ID" });
+router.get("/radius/:radius/:lat/:lon", async (req, res) => {
+  const { radius, lat, lon } = req.params;
+
+  const latitude = parseFloat(lat);
+  if (!latitude) return res.status(400).json({ errorMessage: `Latitude '${ lat }' is not a number.` });
+
+  const longitude = parseFloat(lon);
+  if (!longitude) return res.status(400).json({ errorMessage: `Longitude '${ lon }' is not a number.` });
+
+  try {
+    const locations = await Location.getLocationsByRadius(latitude, longitude, radius);
+    if (locations.length) return res.status(200).json(locations);
+    return res.status(404).json({ errorMessage: "No locations within that radius found" });
+  } catch (e) {
+    return res.status(500).json({ errorMessage: `Error getting location: ${ e }` });
+  }
 });
 
 router.post("/", (req, res) => {
@@ -22,13 +43,18 @@ router.post("/", (req, res) => {
 router.delete("/:id", async (req, res) => {
   const id = req.params.id;
 
-  const location = await Location.getLocationById(id);
+  try {
+    const location = await Location.getLocationById(id);
 
-  if (location) {
-    return User.remove(id)
-      .then(rmvd => res.status(200).json({ delete: "success", location }))
-      .catch(err => res.status(500).json({ errorMessage: `Error deleting location: ${ err }` }));
-  } else return res.status(404).json({ errorMessage: "No location with that ID" });
+    if (location) {
+      return Location.remove(id)
+        .then(rmvd => res.status(200).json({ delete: "success", location }))
+        .catch(err => res.status(500).json({ errorMessage: `Error deleting location: ${ err }` }));
+    }
+    return res.status(404).json({ errorMessage: "No location with that ID" });
+  } catch (e) {
+    return res.status(500).json({ errorMessage: `Error getting location to delete: ${ err }` });
+  }
 });
 
 router.put("/:id", (req, res) => {
