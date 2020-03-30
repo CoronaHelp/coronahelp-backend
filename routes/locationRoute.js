25a7b12169a7562089aaa0de19f3f1639500bdee
Carlos Vargas <cornielleandres@gmail.com>
2020-03-30 18:05:24 -0400

    Add delete and put for locations.

--- routes/locationRoute.js
+++ routes/locationRoute.js
@@ -10,12 +10,32 @@ router.get("/:id", async (req, res) => {
   const location = await Location.getLocationById(id);
 
   if (location) return res.status(200).json(location);
-  else return res.status(404).json({ errorMessage: "No location with that ID" });
+  return res.status(404).json({ errorMessage: "No location with that ID" });
 });
 
 router.post("/", (req, res) => {
   return Location.create(req.body)
     .then(insrtd => res.status(201).json(insrtd))
+    .catch(err => res.status(500).json({ errorMessage: `Error creating location: ${ err }` }));
+});
+
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const location = await Location.getLocationById(id);
+
+  if (location) {
+    return User.remove(id)
+      .then(rmvd => res.status(200).json({ delete: "success", location }))
+      .catch(err => res.status(500).json({ errorMessage: `Error deleting location: ${ err }` }));
+  } else return res.status(404).json({ errorMessage: "No location with that ID" });
+});
+
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+
+  return Location.update(id, req.body)
+    .then(updtd => res.status(201).json({ update: "success", updtd }))
     .catch(err => res.status(500).json(err));
 });
 
