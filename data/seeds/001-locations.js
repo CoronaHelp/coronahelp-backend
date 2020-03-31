92dafb58fa41dd73aae395d4ec5da51cb087f145
Carlos Vargas <cornielleandres@gmail.com>
2020-03-31 15:42:59 -0400

    Fix seeds.

--- data/seeds/001-locations.js
+++ data/seeds/001-locations.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('locations').insert([
-        {id: 1, name: 'Adam\'s Warehouse', address: '123 Adam St.', latitude: '10.123456', longitude: '10.123456' },
-        {id: 2, name: 'Brian\'s Supermarket', address: '123 Brian St.', latitude: '11.123456', longitude: '11.123456' },
-        {id: 3, name: 'Carol\'s Pharmacy', address: '123 Carol St.', latitude: '12.123456', longitude: '12.123456' },
+        {id: 1, name: 'Adam\'s Warehouse', address: '123 Adam St.', latitude: 10.123456, longitude: 10.123456 },
+        {id: 2, name: 'Brian\'s Supermarket', address: '123 Brian St.', latitude: 11.123456, longitude: 11.123456 },
+        {id: 3, name: 'Carol\'s Pharmacy', address: '123 Carol St.', latitude: 12.123456, longitude: 12.123456 },
       ]);
     });
 };
--- data/seeds/005-location-inventory.js
+++ data/seeds/005-location-inventory.js
@@ -6,11 +6,11 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('locationInventory').insert([
         {id: 1, locationID: 1, itemID: 1, availabilityID: 1 },
-        {id: 1, locationID: 1, itemID: 3, availabilityID: 2 },
-        {id: 2, locationID: 2, itemID: 1, availabilityID: 2 },
-        {id: 2, locationID: 2, itemID: 2, availabilityID: 2 },
-        {id: 3, locationID: 3, itemID: 2, availabilityID: 1 },
-        {id: 3, locationID: 3, itemID: 3, availabilityID: 1 },
+        {id: 2, locationID: 1, itemID: 3, availabilityID: 2 },
+        {id: 3, locationID: 2, itemID: 1, availabilityID: 2 },
+        {id: 4, locationID: 2, itemID: 2, availabilityID: 2 },
+        {id: 5, locationID: 3, itemID: 2, availabilityID: 1 },
+        {id: 6, locationID: 3, itemID: 3, availabilityID: 1 },
       ]);
     });
 };
